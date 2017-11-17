68f2106f172e41b84ca779c403bdd15907b3a3f5
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-11-17 13:05:44 -0500

    Created Lab

---
+++ Lab3.2/src/Runner.java
@@ -0,0 +1,27 @@
+
+public class Runner {
+
+	/*
+	  Lamia Islam && Kaitlyn Cao
+	  Period 6 and 7
+	  Description: Creating various array methods and sorting methods
+	 */
+	public static void main (String[]args)
+	{
+
+	}
+	
+	public static String[] merge(String[] list1, String[] list2)
+	{
+		String[] list1 = {"A", "C", "E", "G", "I", "K", "M", "O", "Q", "S", "U", "W", "Y"};
+		String[] list2 = {"B", "D", "F", "H", "J", "L", "N", "P", "R", "T", "V", "X", "Z"};
+		
+		String[] list3  = new String[list1.length + list2.length];
+		
+		for ( int i = o; i < list2.length; i++)
+		{
+			
+		}
+		 
+	}
+} 
