51e4f5403c4f179639b84d41a8692c4b8ff087a3
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-11-20 13:15:44 -0500

    33.3333333% done :')

--- Lab3.2/src/Runner.java
+++ Lab3.2/src/Runner.java
@@ -1,27 +1,69 @@
-
+/**
+  Lamia Islam && Kaitlyn Cao
+  Period 6 and 7
+  Description: Creating various array methods and sorting methods
+ */
 public class Runner {
 
-	/*
-	  Lamia Islam && Kaitlyn Cao
-	  Period 6 and 7
-	  Description: Creating various array methods and sorting methods
-	 */
-	public static void main (String[]args)
-	{
 
-	}
-	
-	public static String[] merge(String[] list1, String[] list2)
+	public static void main (String[]args)
 	{
 		String[] list1 = {"A", "C", "E", "G", "I", "K", "M", "O", "Q", "S", "U", "W", "Y"};
 		String[] list2 = {"B", "D", "F", "H", "J", "L", "N", "P", "R", "T", "V", "X", "Z"};
 		
-		String[] list3  = new String[list1.length + list2.length];
+		String[] list3 = merge(list1,list2);
+		printArr(list3);
 		
-		for ( int i = o; i < list2.length; i++)
+		String[] mslist1 = {"M", "S", "K"};
+		String[] mslist2 = {"C", "L", "I"};
+	}
+	
+	public static String[] merge(String[] list1, String[] list2)
+	{	
+		String[] result  = new String[list1.length + list2.length];
+		int index = 0;
+		for ( int i = 0; i < list1.length && i < list2.length; i++)
+		{
+			result[index] = list1[i] + " " ;
+			result[index+1] = list2[i] + " ";
+			index += 2;
+		}
+		if(list1.length > list2.length)
+		{
+			for(int j = list2.length; j<list2.length; j++)
+			{
+				result[index] = list1[j] + " ";
+			}
+		}
+		
+		else
+		{
+			for(int j = list1.length; j<list2.length; j++)
+			{
+				result[index] = list2[j] + " ";
+			}
+		}
+		return result; 
+	}
+	
+	
+	public static void printArr(String[] arr)
+	{
+		for(int i = 0; i<arr.length; i++)
+		{
+			System.out.print(arr[i]);
+		}
+	}
+	
+	public static String[] mergeSort(String[] list)
+	{
+		if(list.length <= 1)
+		{
+			return list;
+		}
+		else
 		{
 			
 		}
-		 
 	}
 } 
--- Lab3.2/src/Utilities.java
+++ Lab3.2/src/Utilities.java
@@ -1,4 +1,11 @@
 
 public class Utilities {
-
+	
+	public static void printArr(String[] arr)
+	{
+		for(int i = 0; i<arr.length; i++)
+		{
+			System.out.print(arr[i]);;
+		}
+	}
 }
